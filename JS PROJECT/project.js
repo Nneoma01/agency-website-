9902ef11d3c8cb1ac6cd8db25867fbe8e5cb0b0e
Nneoma01 <toughspirit>
2023-07-05 13:29:14 +0100

    new update

---
+++ JS PROJECT/project.js
@@ -0,0 +1,32 @@
+
+const headBar = document.querySelector(".head-bar")
+const headCloseBtn = document.querySelector(".head-bar .close")
+console.log([headCloseBtn])
+
+headCloseBtn.addEventListener("click", e => {
+  headBar.style.display = "none"
+  // headBar.remove()
+  document.body.style.paddingTop = 0
+})
+
+const screens = document.querySelectorAll("img.screens") // []
+
+const changeScreen = (scAttr) => {
+  console.log(`screens would be ${scAttr}`);
+  if(scAttr === "auto") {
+    alert("desktop")
+  } else if(scAttr === "1200px") {
+    alert("laptop")
+  }
+}
+
+
+screens.forEach(sc => {
+  sc.addEventListener("click", e => {
+    const scAttr = sc.getAttribute("data-screen")
+    changeScreen(scAttr)
+  })
+})
+
+const arr =[]
+arr.forEach(e=> console.log(e))
